a04a661fecbbe19f325602ec5d93e1638ec44590
iaksw <33810267+iaksw@users.noreply.github.com>
2018-01-05 17:02:09 +0530

    using l exp 4

---
+++ PrimaryAppropriateBarnowl.java
@@ -0,0 +1,10 @@
+################1st l exp#################
+
+class PrimaryAppropriateBarnowl {
+  public static void main(String[] args) {
+    System.out.println("##############Lamda Exp!##########");
+    
+    Runnable rObj = () -> System.out.println("***Lets begin Lamda Expression on J8!!! ***");
+    rObj.run();
+  }
+}
